3bf940b83d013020c6b1d316ff9b72904a69d08a
Yassir Dahman <dahmanyassir867@gmail.com>
2022-02-11 11:15:55 +0100

    first commit

--- src/Package/Classe1.java
+++ src/Package/Classe1.java
@@ -5,7 +5,7 @@ public class Classe1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 
-		System.out.println("Code and project modified");
+		System.out.println("Code and project re-modified");
 	}
 
 }
